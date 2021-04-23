77e13a6818941f297b2fd48a9bd2d542c39bfddf
yutomorita416 <yutomorita416@gmail.com>
2021-04-23 04:07:12 +0000

    finish アプリケーションに必要なファイルの準備と設定

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,21 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+  
+  def create
+  end
+
+  def edit
+  end
+  
+  def destroy
+  end
+  
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit]
   devise_for :users
   root to: "homes#top"
 end
