54fccedf43584314da8ff98df92a0cd4fbdac493
yutomorita416 <yutomorita416@gmail.com>
2021-04-24 14:19:41 +0000

    add マイページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,11 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
   
   def update
