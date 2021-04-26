85f4cf9c8ed35fd907904b3820d249c412d14b3b
yutomorita416 <yutomorita416@gmail.com>
2021-04-26 09:28:42 +0000

    finish book-index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -10,28 +11,31 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(book.id)
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
   end
-  
+
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
-  
-  
+
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
