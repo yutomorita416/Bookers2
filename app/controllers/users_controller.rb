bc7bdf387a6532e3a328d7aa670d8a07fb44d497
yutomorita416 <yutomorita416@gmail.com>
2021-04-26 14:06:54 +0000

    finish フラッシュメッセージの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,11 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+    end
   end
 
 
