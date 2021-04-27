a718db5df2ca3b167d200b78bc2888c152bd6a26
yutomorita416 <yutomorita416@gmail.com>
2021-04-27 04:07:27 +0000

    finish アクセス制限の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+ 
   def index
     @users = User.all
     @book = Book.new
@@ -12,6 +13,11 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to user_path(@current_user.id)
+    end
   end
 
   def update
